6d987716a90f2caba6a74e1e2c190814651c8d99
Gibartes <konm2000@hotmail.com>
2025-10-02 22:56:04 +0900

    Update product options

--- Tools/build.ps1
+++ Tools/build.ps1
@@ -1,3 +1,7 @@
-cd ..\TinyCam
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=false --self-contained true
-cd ..
+Set-Variable -Name "currentDir" -Value (Get-Location).Path
+Push-Location
+Set-Location ..\TinyCam
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --self-contained true -p:OutputType=WinExe
+Write-Output "Path of the released files:"
+Write-Output "${currentDir}\..\TinyCam\bin\Release\net8.0\win-x64\publish\"
+Pop-Location
